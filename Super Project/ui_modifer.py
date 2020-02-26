71b252e9078010ea83ea1199d2958af884b36425
Leonid Litvinov <leonidlitvinoff@yandex.ru>
2020-02-26 18:30:24 +0700

    Code refactored and query function edited

--- Super Project/ui_modifer.py
+++ Super Project/ui_modifer.py
@@ -2,9 +2,6 @@ from PyQt5.QtWidgets import QWidget, QApplication
 from PyQt5.QtWidgets import QErrorMessage
 from PyQt5.QtGui import QPixmap
 from PIL.ImageQt import ImageQt
-from PyQt5.QtGui import QCursor
-from PyQt5 import QtCore
-from PyQt5 import QtGui
 from PIL import Image
 from io import BytesIO
 import requests
@@ -22,9 +19,12 @@ class Widget(QWidget, Ui_Form):
         self.setupUi(self)
         self.horizontalSlider.valueChanged.connect(self.changeZ)
         self.pushButton.clicked.connect(self.query)
+        self.horizontalSlider.sliderReleased.connect(self.query)
+
+        self.exc = None
 
     def changeZ(self):
-        self.label_2.setText(f'Масштаб: {str(self.horizontalSlider.value())}')
+        self.label_2.setText(f'Масштаб: {self.horizontalSlider.value()}')
 
     def query(self):
         map_request = {
@@ -36,6 +36,8 @@ class Widget(QWidget, Ui_Form):
             url = 'http://static-maps.yandex.ru/1.x/'
             request = requests.get(url, params=map_request)
             self.label_3.setPixmap(QPixmap.fromImage(ImageQt(Image.open(BytesIO(request.content)))))
+            self.exc = None
         except Exception as e:
-            self.exc = QErrorMessage()
-            self.exc.showMessage(str(e).capitalize())
+            if self.exc is None:
+                self.exc = QErrorMessage()
+                self.exc.showMessage(str(e).capitalize())
