e68c3ababcfc2a620d7dc471b2eb1c247080bdc2
stf2003.kem@gmail.com <bpKz!UH$uDRZzoTiCnaQ66cetx7a*QALGG%9wgcnM0H%b4S^mPnAd6#MiAT!3k0T&v7zsN@N>
2020-02-26 19:05:14 +0700

    Error fixed

--- Super Project/ui_modifer.py
+++ Super Project/ui_modifer.py
@@ -6,8 +6,8 @@ from PIL import Image
 from io import BytesIO
 import requests
 
-
 import sys
+
 sys.path.append('data\\ui-py\\')
 
 from mainWindow import Ui_Form
@@ -17,27 +17,60 @@ class Widget(QWidget, Ui_Form):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        self.horizontalSlider.valueChanged.connect(self.changeZ)
+        self.horizontalSlider.valueChanged.connect(self.changeZoom)
         self.pushButton.clicked.connect(self.query)
         self.horizontalSlider.sliderReleased.connect(self.query)
 
-        self.exc = None
+        self.zoom = 0
+        self.error_msg = QErrorMessage()
 
-    def changeZ(self):
-        self.label_2.setText(f'Масштаб: {self.horizontalSlider.value()}')
+    def changeZoom(self, z):
+        self.zoom = z
+        self.label_2.setText(f'Масштаб: {z}')
 
     def query(self):
-        map_request = {
-            'l': 'map',
-            'z': str(self.horizontalSlider.value())
-        }
+        map_request = {'l': 'map', 'z': str(self.zoom)}
+        w, h = self.lineEdit.text(), self.lineEdit_2.text()
+        print(1)
+        if self.is_valid(w, h):
+            map_request['ll'] = f'{w},{h}'
+        else:
+            return
+        print(2)
+        url = 'http://static-maps.yandex.ru/1.x/'
+        request = requests.get(url, params=map_request)
+        self.label_3.setPixmap(QPixmap.fromImage(ImageQt(Image.open(BytesIO(request.content)))))
+
+    def is_valid(self, w, h, show_error=True, debag=False):
+        if debag:
+            print('перевод долготы в float')
         try:
-            map_request['ll'] = f'{float(self.lineEdit.text())},{float(self.lineEdit_2.text())}'
-            url = 'http://static-maps.yandex.ru/1.x/'
-            request = requests.get(url, params=map_request)
-            self.label_3.setPixmap(QPixmap.fromImage(ImageQt(Image.open(BytesIO(request.content)))))
-            self.exc = None
-        except Exception as e:
-            if self.exc is None:
-                self.exc = QErrorMessage()
-                self.exc.showMessage(str(e).capitalize())
+            w = float(self.lineEdit.text())
+        except Exception:
+            self.error('Неверная долгота (выражается вещественным числом, через точку)')
+            return
+
+        if debag:
+            print('проверка ограничения долготы (-180 - 180)')
+        if w >= 180 or w <= -180:
+            self.error('Неверная долгота (от -180 до 180)')
+            return
+
+        if debag:
+            print('перевод широта в float')
+        try:
+            h = float(self.lineEdit.text())
+        except Exception:
+            self.error('Неверная широта (выражается вещественным числом, через точку)')
+            return
+
+        if debag:
+            print('проверка ограничения долготы (-90 - 90)')
+        if h >= 90 or h <= -90:
+            self.error('Неверная широта (от -90 до 90)')
+            return
+
+        return str(w), str(h)
+
+    def error(self, msg):
+        self.error_msg.showMessage(msg)
