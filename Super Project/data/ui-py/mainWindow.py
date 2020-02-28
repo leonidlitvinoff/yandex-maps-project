01a6096d6838aefe49cb0cfd76df80bb68364f92
Леонид Литвинов <leonidlitvinoff@yandex.ru>
2020-02-28 19:45:14 +0700

    Added possibility to move map with Left, Right, Down and Up control arrows.

--- Super Project/data/ui-py/mainWindow.py
+++ Super Project/data/ui-py/mainWindow.py
@@ -1,14 +1,12 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'design.ui'
+# Form implementation generated from reading ui file 'mainWindow.ui'
 #
-# Created by: PyQt5 UI code generator 5.13.0
+# Created by: PyQt5 UI code generator 5.9.2
 #
 # WARNING! All changes made in this file will be lost!
 
-
 from PyQt5 import QtCore, QtGui, QtWidgets
-
 
 class Ui_Form(object):
     def setupUi(self, Form):
@@ -21,6 +19,16 @@ class Ui_Form(object):
         self.label_3.setAlignment(QtCore.Qt.AlignCenter)
         self.label_3.setObjectName("label_3")
         self.gridLayout_2.addWidget(self.label_3, 1, 0, 1, 3)
+        self.pushButton = QtWidgets.QPushButton(Form)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Maximum, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.pushButton.sizePolicy().hasHeightForWidth())
+        self.pushButton.setSizePolicy(sizePolicy)
+        self.pushButton.setObjectName("pushButton")
+        self.gridLayout_2.addWidget(self.pushButton, 0, 2, 1, 1)
+        spacerItem = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Minimum)
+        self.gridLayout_2.addItem(spacerItem, 0, 1, 1, 1)
         self.gridLayout = QtWidgets.QGridLayout()
         self.gridLayout.setObjectName("gridLayout")
         self.label_2 = QtWidgets.QLabel(Form)
@@ -82,16 +90,27 @@ class Ui_Form(object):
         self.horizontalSlider.setObjectName("horizontalSlider")
         self.gridLayout.addWidget(self.horizontalSlider, 1, 1, 1, 4)
         self.gridLayout_2.addLayout(self.gridLayout, 0, 0, 1, 1)
-        self.pushButton = QtWidgets.QPushButton(Form)
-        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Maximum, QtWidgets.QSizePolicy.Fixed)
+        self.horizontalLayout = QtWidgets.QHBoxLayout()
+        self.horizontalLayout.setSizeConstraint(QtWidgets.QLayout.SetMinimumSize)
+        self.horizontalLayout.setSpacing(6)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.pushButton_2 = QtWidgets.QPushButton(Form)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Maximum, QtWidgets.QSizePolicy.Maximum)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.pushButton.sizePolicy().hasHeightForWidth())
-        self.pushButton.setSizePolicy(sizePolicy)
-        self.pushButton.setObjectName("pushButton")
-        self.gridLayout_2.addWidget(self.pushButton, 0, 2, 1, 1)
-        spacerItem = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Minimum)
-        self.gridLayout_2.addItem(spacerItem, 0, 1, 1, 1)
+        sizePolicy.setHeightForWidth(self.pushButton_2.sizePolicy().hasHeightForWidth())
+        self.pushButton_2.setSizePolicy(sizePolicy)
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.horizontalLayout.addWidget(self.pushButton_2)
+        self.label_6 = QtWidgets.QLabel(Form)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Maximum)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.label_6.sizePolicy().hasHeightForWidth())
+        self.label_6.setSizePolicy(sizePolicy)
+        self.label_6.setObjectName("label_6")
+        self.horizontalLayout.addWidget(self.label_6)
+        self.gridLayout_2.addLayout(self.horizontalLayout, 2, 0, 1, 1)
 
         self.retranslateUi(Form)
         QtCore.QMetaObject.connectSlotsByName(Form)
@@ -99,8 +118,11 @@ class Ui_Form(object):
     def retranslateUi(self, Form):
         _translate = QtCore.QCoreApplication.translate
         Form.setWindowTitle(_translate("Form", "Form"))
+        self.pushButton.setText(_translate("Form", "Найти"))
         self.label_2.setText(_translate("Form", "Масштаб: 0"))
         self.label.setText(_translate("Form", "Координаты:"))
         self.label_4.setText(_translate("Form", "Широта:"))
         self.label_5.setText(_translate("Form", "Долгота:"))
-        self.pushButton.setText(_translate("Form", "Найти"))
+        self.pushButton_2.setText(_translate("Form", "Управление картой"))
+        self.label_6.setText(_translate("Form", "(для выхода нажмите Escape)"))
+
--- Super Project/ui_modifer.py
+++ Super Project/ui_modifer.py
@@ -6,6 +6,7 @@ from PIL import Image
 from io import BytesIO
 import requests
 from PyQt5.QtGui import QKeyEvent
+from PyQt5.QtCore import Qt
 
 import sys
 
@@ -20,35 +21,87 @@ class Widget(QWidget, Ui_Form):
         self.setupUi(self)
         self.horizontalSlider.valueChanged.connect(self.changeZoom)
         self.pushButton.clicked.connect(self.query)
+        self.pushButton_2.clicked.connect(self.editMoveMapMode)
         self.horizontalSlider.sliderReleased.connect(self.query)
-
+        self.w = 0.0
+        self.h = 0.0
+        self.move_map_mode = False
         self.zoom = 0
         self.error_msg = QErrorMessage()
+        self.zoom_map = {0: 18,
+                         1: 24,
+                         2: 18,
+                         3: 13,
+                         4: 9,
+                         5: 1.3,
+                         6: 0.9,
+                         7: 0.5,
+                         8: 0.1,
+                         9: 0.068,
+                         10: 0.03,
+                         11: 0.009,
+                         12: 0.005,
+                         13: 0.001,
+                         14: 0.0006,
+                         15: 0.0002,
+                         16: 0.00008,
+                         17: 0.000046}
+
+    def editMoveMapMode(self, value=False):
+        self.lineEdit.setEnabled(value)
+        self.lineEdit_2.setEnabled(value)
+        self.horizontalSlider.setEnabled(value)
+        self.pushButton.setEnabled(value)
+        self.pushButton_2.setEnabled(value)
+        self.move_map_mode = not value
+
+
 
     def changeZoom(self, z):
         self.zoom = z
         self.label_2.setText(f'Масштаб: {z}')
 
     def keyPressEvent(self, a0: QKeyEvent) -> None:
-        if a0.key() == 16777238:
+        if a0.key() == Qt.Key_PageUp:
             if self.zoom < 17:
                 self.zoom += 1
-        elif a0.key() == 16777239:
+                self.horizontalSlider.setValue(self.zoom)
+                self.query()
+        elif a0.key() == Qt.Key_PageDown:
             if self.zoom > 0:
                 self.zoom -= 1
-        self.horizontalSlider.setValue(self.zoom)
-        self.query()
+                self.horizontalSlider.setValue(self.zoom)
+                self.query()
+        elif a0.key() == Qt.Key_Escape:
+            if self.move_map_mode:
+                self.editMoveMapMode(True)
+        else:
+            if self.move_map_mode:
+                if a0.key() == Qt.Key_Up:
+                    self.h += self.zoom_map[self.zoom]
+                    self.query()
+                elif a0.key() == Qt.Key_Down:
+                    self.h -= self.zoom_map[self.zoom]
+                    self.query()
+                elif a0.key() == Qt.Key_Left:
+                    self.w -= self.zoom_map[self.zoom]
+                    self.query()
+                elif a0.key() == Qt.Key_Right:
+                    self.w += self.zoom_map[self.zoom]
+                    self.query()
 
     def query(self):
         map_request = {'l': 'map', 'z': str(self.zoom)}
         w, h = self.lineEdit.text(), self.lineEdit_2.text()
         if self.is_valid(w, h):
-            map_request['ll'] = f'{w},{h}'
+            map_request['ll'] = f'{float(w) + self.w},{float(h) + self.h}'
         else:
             return
         url = 'http://static-maps.yandex.ru/1.x/'
         request = requests.get(url, params=map_request)
+        print(3)
         self.label_3.setPixmap(QPixmap.fromImage(ImageQt(Image.open(BytesIO(request.content)))))
+        print(4)
 
     def is_valid(self, w, h, show_error=True, debag=False):
         if debag:
